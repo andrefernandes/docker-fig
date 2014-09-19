326136c15d33776302969075e60c602b28aa1182
andre <goroh@hotmail.com>
2014-09-19 02:31:55 -0300

    runfig fix

--- runfig.sh
+++ runfig.sh
@@ -2,5 +2,5 @@
 docker run --rm -t -i \
    -v /var/run/docker.sock:/var/run/docker.sock \
    -v /usr/bin/docker:/usr/bin/docker \
-   andrefernandes/docker-fig:latest $ENVBASH ${@}
+   andrefernandes/docker-fig:latest ${@}
 
